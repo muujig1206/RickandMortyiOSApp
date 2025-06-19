151afcb1f9d7370e8be1218ee68ce0cfac33746f
Munkh-Au <n.munkhanu@gmail.com>
2025-06-19 11:53:23 +0800

    Initial Commit

---
+++ RickandMortyTests/RickandMortyTests.swift
@@ -0,0 +1,17 @@
+//
+//  RickandMortyTests.swift
+//  RickandMortyTests
+//
+//  Created by Munkh-Anu Nyamdorj on 2025.06.19.
+//
+
+import Testing
+@testable import RickandMorty
+
+struct RickandMortyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
