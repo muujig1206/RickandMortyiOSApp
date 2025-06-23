640148c975b02bf0b8c7bd495fd12bb5f552734c
Munkh-Au <n.munkhanu@gmail.com>
2025-06-23 13:22:10 +0800

    Added models

--- RickandMorty/APIClient/RMEndpoint.swift
+++ RickandMorty/APIClient/RMEndpoint.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 ///Represenst unique API endpoint
-@frozen enum RMEndpoint: String {
+enum RMEndpoint: String {
     /// Endpoint to get character info
     case character
     /// Endpoint to get location info
